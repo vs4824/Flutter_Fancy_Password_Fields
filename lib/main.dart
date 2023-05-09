00237aed8e2936bfea9d6af6954b733b68eec375
Vaibhav Sharma <vaibhav.2022@tecorb.co>
2023-05-09 16:36:19 +0530

    Flutter Fancy Password Fields

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'samples/samples.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            /// Change this to one of the available samples on the samples folder
+            Sample1(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/samples/samples.dart
@@ -0,0 +1,5 @@
+export 'sample_initial_value.dart';
+export 'sample1.dart';
+export 'sample2.dart';
+export 'sample3.dart';
+export 'sample4.dart';
